1b0ad2c17258624bf94a0ecf565a8bc0b40abcbc
DUREL <149640202+Durel1@users.noreply.github.com>
2025-05-26 22:37:43 +0100

    starting the body

---
+++ src/Components/main.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../Styles/Components_styles/main.css"; 
+
+const Main = () => {
+  return (
+    <div className="job-search-container">
+      <div className="top-buttons">
+        <button className="candidate-button">
+          Candidat : Déposez votre CV
+        </button>
+
+        <button className="recruiter-button">
+          Recruteur : Publiez une annonce
+        </button> 
+      </div>
+
+      <div className="search-box">
+        <h2 className="search-title">
+          Trouvez votre futur job parmi <span className="highlight">2056</span> postes ouverts
+        </h2>
+        <div className="search-inputs">
+          <input
+            type="text"
+            placeholder="Mots-clés"
+            className="input"
+          />
+          <select className="input">
+            <option>Région(s)</option>
+          </select>
+          <select className="input">
+            <option>Métier</option>
+          </select>
+          <button className="search-button">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 103.6 3.6a7.5 7.5 0 0013.05 13.05z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+    
+  );
+};
+
+export default Main;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import Main from '../Components/main'
 
 function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Welcome to GetJob</h1>
+
+      <Main />
 
       <Footer />
     </div>
